3b50c25d4d7ba030a4cd8f7de60bafb0afa7b939
jacklee8899 <150518425+jacklee8899@users.noreply.github.com>
2023-11-12 12:23:47 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    print("Please enter number of tickets: ")
+    val totalTicket : Int = scanner.nextInt()
+
+    print("How many round-trip tickets: ")
+    val roundTripTicket : Int = scanner.nextInt()
+
+    val tester = Tester(totalTicket, roundTripTicket)
+    tester.print()
+}
+
+class Tester(var totalTicket:Int, var roundTripTicket:Int) {
+    fun print() {
+        val totalsum : Int = ((totalTicket-roundTripTicket)*1000 + roundTripTicket * 2000 * 90 /100)
+
+        println("Total tickets: " + totalTicket +
+                " Round-trip: " + roundTripTicket +
+                " Total: " + totalsum)
+    }
 }
